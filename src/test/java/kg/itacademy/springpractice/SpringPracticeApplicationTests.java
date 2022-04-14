41b07258d16b4f51e4743a135e31e14b979680c6
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-04-14 20:44:39 +0600

    Home work

---
+++ src/test/java/kg/itacademy/springpractice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.itacademy.springpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
